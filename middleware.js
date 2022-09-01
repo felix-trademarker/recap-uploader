944829816ec4260e560695969e0294350c0ec0be
Felix <felix.bongo@tweeklabs.com>
2022-09-01 10:56:57 +0800

    update

--- middleware.js
+++ middleware.js
@@ -1,9 +1,16 @@
+var axios = require('axios')
 
+// secret: '71e0aba070df4892e7384da1828fbfff',
+// name: 'cpod.sid',
 
 exports.verify = function(req, res, next){
     let accessToken = req.cookies.jwt
     const { userId } = req.session
 
     console.log("userId",userId)
-    console.log("accessToken",accessToken)
+    console.log("accessToken",req.cookies)
+
+    // let results = axios.get()
+
+    next()
 }
