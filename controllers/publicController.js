5ee8bb3c4eb068c0934ecf0d7d50619284d0aa61
Felix <felix.bongo@tweeklabs.com>
2022-09-20 10:26:26 +0800

    update

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -25,51 +25,10 @@ exports.index = async function(req, res, next) {
     let recaps = await rpoRecaps.get()
 
     // TEST SMS START >>>>>>
-    const path = require ("path");
-    const fs = require ("fs");
-    const fetch = require ("node-fetch")
-    
-
-    // let dest = "+886900328388", message="this is a test message from telnyx";
-    // // function sendSMS (dest, message) {
-    //   const url = 'https://sms.telnyx.com/messages';
-    //   var apiKey, profile, profileSecret;
-    
-    //   // second first profile: 
-    //   profile = "ddd9a7c4-c3fc-43e8-a0dd-e5bb462d38e4";
-    //   profileSecret = "erxjRp55M5XB3MGfxpxmz9yy";
-    //   apiKey = "KEY01808D0DBDBA586F1FFB803D2D7F0E24_0ezcsARVzAE9DEgfEmnRdK";
-    
-    //   let body = {
-    //     src: "19176942023",
-    //     dst: dest,
-    //     is_mms: false,
-    //     body: message,
-    //     original_body: message,
-    // //    "delivery_status_webhook_url": "https://api.bigfoot.com/webhook/sms"
-    //   }
-    
-    
-    //   fetch (url, {
-    //       headers: { 'Accept': 'application/json', 'X-Profile-Secret': profileSecret,
-    //         'Content-Type': 'application/json' }, 
-    //       method: "POST", 
-    //       body: JSON.stringify (body) } 
-    //   )
-    //   .catch (function (res) { console.log (res); })
-    //   .then (result => result.text ())
-    //   .then (answer => {
-    //      console.log ("Response from SMS API:")
-    //      console.log (answer);
-    //      if (answer.success == false) { 
-    //       let message = "BF: Problem sending SMS";
-    //       if (typeof answer.message == "string") message = answer.message;
-    //       sa.warning ("baidu/download/sms", message);
-    //      }
-    //   });
+ 
 
     
-// TEST SMS END <<<<<<
+    // TEST SMS END <<<<<<
 
 
     // console.log(lessons);
